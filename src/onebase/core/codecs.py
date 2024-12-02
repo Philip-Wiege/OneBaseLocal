4539adc4df3cde6fca6a29a898bd849352f76d4f
Pillow <Philip.Wiege@googlemail.com>
2024-12-02 14:27:52 +0100

    Update codecs.py

--- src/onebase/core/codecs.py
+++ src/onebase/core/codecs.py
@@ -2,7 +2,7 @@ import udsoncan
 from typing import Optional, Any
 import datetime
 import json
-import enumerations
+from enumerations import *
 
 class CodecRaw(udsoncan.DidCodec):
     def __init__(self, paramNumBytes: int, paramDIDName:str):
