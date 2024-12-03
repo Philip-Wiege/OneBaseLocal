00c863ef9dff47c511c90d0dc34e289f870b9c09
Pillow <Philip.Wiege@googlemail.com>
2024-12-03 20:27:57 +0100

    updated example file for codecs

--- src/onebase/example_codecs.py
+++ src/onebase/example_codecs.py
@@ -1,3 +1,8 @@
-from core.codecs import *
+import onebase as ob
+import onebase.core as co
+import onebase.core.codecs as cd
+import onebase.uds as uds
 
-exampleCodec = CodecInt8(2, "DomesticHotWaterTemperatureSetpoint")
+InstanceCodecInt16 = cd.CodecInt16(paramNumBytes=2, paramDIDName="DomesticHotWaterTemperatureSetpoint", paramSigned=True, paramScale=1.0)
+InstanceCodecInt32 = cd.CodecInt32(paramNumBytes=4, paramDIDName="TotalElectricalEnergy", paramSigned=False, paramScale=10)
+InstanceCodecUTF8 = cd.CodecUTF8(paramNumBytes=40, paramDIDName="CentralHeatingOneCircuitName")
