c44d2945eaed4686adeb8a6e8c7f75b9eaf14717
Pillow <Philip.Wiege@googlemail.com>
2025-01-15 15:04:00 +0100

    changed to JSON file

--- src/onebase/example_ecu_connection.py
+++ src/onebase/example_ecu_connection.py
@@ -1,3 +1,5 @@
 from onebase.core.ecu_connection import ECUConnection
 
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramDoIPAddress="192.168.0.1")
+#InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramDoIPAddress="192.168.0.1")
+
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramFilepathDIDList="BV_OneBase_DIDs.json")
