e0f3a216143d83b361de10441d44256b5d8a8928
Philip Wiege <Philip.Wiege@googlemail.com>
2025-01-16 10:18:42 +0100

    added multiple connection types

--- src/onebase/core/ecu_connection.py
+++ src/onebase/core/ecu_connection.py
@@ -35,20 +35,22 @@ def import_path(path):
     return module
 
 class ECUConnection():
-    def __init__(self, paramTXAddress:int=0x680, paramRXAddress:int=None, paramDoIPAddress:str=None, can:str='can0', paramFilepathDIDList:str=""):
-
+    def __init__(self, paramTXAddress:int=0x680, paramRXAddress:int=None, paramConnectionType:str=None, paramConnectionInterface:str=None, paramFilepathDIDList:str=""):
+        # calculate RX address
         self.tx = paramTXAddress
         if paramRXAddress == None:
             self.rx = paramTXAddress + 0x10
         else:
             self.rx = paramRXAddress
 
+        # load DID definition file
         self.dataIdentifiers = self._loadDIDFile(paramFilePath=paramFilepathDIDList)       
 
-        # select CAN / DoIP
-        if(paramDoIPAddress != None):
-            conn = DoIPClientUDSConnector(DoIPClient(paramDoIPAddress, self.tx))
-        else:
+        # select backend
+        if(paramConnectionType == "DoIP"): # DoIP
+            conn = DoIPClientUDSConnector(DoIPClient(paramConnectionInterface, self.tx))
+
+        elif (paramConnectionType == "SLCAN"): # SLCAN = CAN over Serial Interface
             # Refer to isotp documentation for full details about parameters
             isotp_params = {
                 'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
@@ -68,12 +70,63 @@ class ECUConnection():
                 'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
                 'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
             }
-            #bus = SocketcanBus(channel=can, bitrate=250000)                                     # Link Layer (CAN protocol)
-            bus = slcanBus(channel="COM3", tty_baudrate=115200, bitrate=250000)
+            bus = slcanBus(channel=paramConnectionInterface, tty_baudrate=115200, bitrate=250000)
             tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=self.tx, rxid=self.rx) # Network layer addressing scheme
             stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
             stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
             conn = PythonIsoTpConnection(stack)                                                 # interface between Application and Transport layer
+
+        elif (paramConnectionType == "SocketCAN"): # SocketCAN Interface on Linux Systems
+            # Refer to isotp documentation for full details about parameters
+            isotp_params = {
+                'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+                'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
+                'wftmax': 0,                            # Number of wait frame allowed before triggering an error
+                'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
+                'tx_data_min_length': 8,                # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
+                'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
+                'rx_flowcontrol_timeout': 1000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
+                'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
+                'override_receiver_stmin': None,        # When sending, respect the stmin requirement of the receiver if set to None.
+                'max_frame_size': 4095,                 # Limit the size of receive frame.
+                'can_fd': False,                        # Does not set the can_fd flag on the output CAN messages
+                'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
+                'rate_limit_enable': False,             # Disable the rate limiter
+                'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
+                'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
+                'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
+            }
+            bus = SocketcanBus(channel=paramConnectionInterface, bitrate=250000)                                     # Link Layer (CAN protocol)
+            tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=self.tx, rxid=self.rx) # Network layer addressing scheme
+            stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
+            stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
+            conn = PythonIsoTpConnection(stack)
+        
+        else: # SocketCAN Interface on Linux Systems as default
+            # Refer to isotp documentation for full details about parameters
+            isotp_params = {
+                'stmin': 10,                            # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+                'blocksize': 0,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
+                'wftmax': 0,                            # Number of wait frame allowed before triggering an error
+                'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
+                'tx_data_min_length': 8,                # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
+                'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
+                'rx_flowcontrol_timeout': 1000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
+                'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
+                'override_receiver_stmin': None,        # When sending, respect the stmin requirement of the receiver if set to None.
+                'max_frame_size': 4095,                 # Limit the size of receive frame.
+                'can_fd': False,                        # Does not set the can_fd flag on the output CAN messages
+                'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
+                'rate_limit_enable': False,             # Disable the rate limiter
+                'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
+                'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
+                'listen_mode': False                    # Does not use the listen_mode which prevent transmission.
+            }
+            bus = SocketcanBus(channel="can0", bitrate=250000)                                     # Link Layer (CAN protocol)
+            tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=self.tx, rxid=self.rx) # Network layer addressing scheme
+            stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)               # Network/Transport layer (IsoTP protocol)
+            stack.set_sleep_timing(0.01, 0.01)                                                  # Balancing speed and load
+            conn = PythonIsoTpConnection(stack)
 
         # configuration for udsoncan client
         config = dict(udsoncan.configs.default_client_config)
--- src/onebase/example_ecu_connection.py
+++ src/onebase/example_ecu_connection.py
@@ -1,5 +1,7 @@
 from onebase.core.ecu_connection import ECUConnection
 
-#InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramDoIPAddress="192.168.0.1")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="DoIP", paramConnectionInterface="192.168.0.1", paramFilepathDIDList="BV_OneBase_DIDs.json")
 
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="COM3", paramFilepathDIDList="BV_OneBase_DIDs.json")
+
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SocketCAN", paramConnectionInterface="can0", paramFilepathDIDList="BV_OneBase_DIDs.json")
