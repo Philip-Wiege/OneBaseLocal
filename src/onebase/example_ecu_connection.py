a4bd285c4730e9973d5a163bf1b03d24c863d56d
Philip Wiege <Philip.Wiege@googlemail.com>
2025-01-16 10:52:20 +0100

    added MacOS serial address

--- src/onebase/example_ecu_connection.py
+++ src/onebase/example_ecu_connection.py
@@ -1,7 +1,13 @@
 from onebase.core.ecu_connection import ECUConnection
 
+# DoIP: Linux, Windows, MacOS
 InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="DoIP", paramConnectionInterface="192.168.0.1", paramFilepathDIDList="BV_OneBase_DIDs.json")
 
+# SLCAN: Windows
 InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="COM3", paramFilepathDIDList="BV_OneBase_DIDs.json")
 
+# SLCAN: MacOS
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="/dev/tty.usbmodem2085309C53301", paramFilepathDIDList="BV_OneBase_DIDs.json")
+
+# SocketCAN: Linux
 InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SocketCAN", paramConnectionInterface="can0", paramFilepathDIDList="BV_OneBase_DIDs.json")
