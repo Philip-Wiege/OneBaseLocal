47fb9b6d3028a5f59b7310b93e19ac241d0a9b84
Pillow <Philip.Wiege@googlemail.com>
2025-01-17 12:20:34 +0100

    fixed relative paths

--- src/onebase/examples/example_ecu_connection.py
+++ src/onebase/examples/example_ecu_connection.py
@@ -1,16 +1,16 @@
 from onebase.core.ecu_connection import ECUConnection
 
 # DoIP: Linux, Windows, MacOS
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="DoIP", paramConnectionInterface="192.168.0.1", paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="DoIP", paramConnectionInterface="192.168.0.1", paramFilepathDIDList="../did_definitions/BV_OneBase_DIDs.json")
 
 # SLCAN: Windows
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="COM3", paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="COM3", paramFilepathDIDList="../did_definitions/BV_OneBase_DIDs.json")
 
 # SLCAN: MacOS
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="/dev/tty.usbmodem2085309C53301", paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SLCAN", paramConnectionInterface="/dev/tty.usbmodem2085309C53301", paramFilepathDIDList="../did_definitions/BV_OneBase_DIDs.json")
 
 # SLCAN Remote Telnet: Linux, Windows, MacOS
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="Telnet", paramConnectionInterface="rfc2217://10.0.1.137:5000", paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="Telnet", paramConnectionInterface="rfc2217://10.0.1.137:5000", paramFilepathDIDList="../did_definitions/BV_OneBase_DIDs.json")
 
 # SocketCAN: Linux
-InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SocketCAN", paramConnectionInterface="can0", paramFilepathDIDList="BV_OneBase_DIDs.json")
+InstanceECUConnection680 = ECUConnection(paramTXAddress=0x680, paramRXAddress=0x690, paramConnectionType="SocketCAN", paramConnectionInterface="can0", paramFilepathDIDList="../did_definitions/BV_OneBase_DIDs.json")
